ea4c4cd201ef26b1ccb8a1cb33aa4684e9cfcd61
Pritika Acharya <pritika136@gmail.com>
2022-03-11 15:27:53 +0530

    some changes

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -6,7 +6,7 @@ function MeetupDetail(props) {
   return (
     <Fragment>
       <section className={classes.detail}>
-        <Image src={props.image} alt={props.title} />
+        <img src={props.image} alt={props.title} />
         <h1>{props.title}</h1>
         <address>{props.address}</address>
         <p>{props.description}</p>
